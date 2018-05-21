9a190e6faa65204c0033c0b331c9308b8cdec1de
Damian Hamill <damianham@gmail.com>
2018-05-21 11:57:55 +0100

    encode model as json for websocket and handle Bytes

--- react/preact_streaming_redux/app/src/sockets/model_socket.cr
+++ react/preact_streaming_redux/app/src/sockets/model_socket.cr
@@ -7,25 +7,51 @@ struct ModelSocket < Maze::WebSockets::ClientSocket
     true
   end
 
-  def self.send_model(action : String, json_model : String)
-    self.broadcast("message", "model", action, {"data" => json_model} )
+  def self.send_model(topic : String, action : String, json_model : String)
+    self.broadcast("message", topic, action, {"data" => json_model} )
   rescue ex
     puts "Error broadcasting model #{ex.message}"
   end
 
-  def self.send_model(action : String, hash_model : Hash(String,DB::Any))
-    self.broadcast("message", "model", action, {"data" => hash_model.to_s} )
-  rescue ex
-    puts "Error broadcasting model #{ex.message}"
-  end
 end
 
 module Sockets::Model
-  def self.send_model(action : String, json_model : String)
-    ModelSocket.send_model(action, json_model)
+
+  def self.json_encode_field(json, col, value)
+    case value
+    when Bytes
+      # custom json encoding. Avoid extra allocations.
+      json.field col do
+        json.array do
+          value.each do |e|
+            json.scalar e
+          end
+        end
+      end
+    else
+      # encode the value as their built in json format.
+      json.field col do
+        value.to_json(json)
+      end
+    end
   end
 
-  def self.send_model(action : String, hash_model : Hash(String,DB::Any))
-    ModelSocket.send_model(action, hash_model)
+  def self.send_model(model_name : String, action : String, json_model : String)
+    ModelSocket.send_model(model_name, action, json_model)
+  end
+
+  def self.send_model(model_name : String, action : String, hash_model : Hash(String,DB::Any))
+    # JSON encode and handle Bytes (Slice(Uint8))
+    io = IO::Memory.new(4096_i64)
+
+    JSON.build(io) do |json|
+      json.object do
+        hash_model.each do |k, v|
+          json_encode_field json, k, v
+        end
+      end
+    end
+
+    ModelSocket.send_model(model_name, action, io.to_s)
   end
 end
